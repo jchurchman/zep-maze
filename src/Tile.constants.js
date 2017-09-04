e220e2e41ecaf774953627e115dbdd563549c9de
Joe Churchman <churchman.joe@gmail.com>
2017-09-04 16:00:44 -0700

    add tile styling

---
+++ src/Tile.constants.js
@@ -0,0 +1,4 @@
+export const FLOOR = 'FLOOR';
+export const WALL = 'WALL';
+export const START = 'START';
+export const END = 'END';
--- src/Tile.js
+++ src/Tile.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { FLOOR, WALL, START, END } from './Tile.constants';
+import styles from './Tile.css';
 
 export function Tile({ value }) {
-
-
-    return (
-        <td></td>
-    )
+    switch(value) {
+        case WALL: return (<td className={styles.wall}></td>);
+        case FLOOR: return (<td className={styles.floor}></td>);
+        case START: return (<td className={styles.start}>{START}</td>);
+        case END: return (<td className={styles.end}>{END}</td>);
+        default: return (<td></td>);
+    }
 }
