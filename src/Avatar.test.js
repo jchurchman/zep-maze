f002fac06a5ac70660cc30995baa7e9f40f45216
Joe Churchman <churchman.joe@gmail.com>
2017-09-04 15:31:01 -0700

    avatar position tests pass

--- src/Avatar.test.js
+++ src/Avatar.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import toJSON from 'enzyme-to-json';
 import { Avatar } from './Avatar';
+import { UP, DOWN, LEFT, RIGHT } from './Avatar.constants';
 
 describe ('Avatar Component', () => {
 
@@ -10,5 +11,23 @@ describe ('Avatar Component', () => {
         expect(toJSON(wrapper)).toMatchSnapshot();
     })
 
+    it('renders a left pointing emoji when given left stance', () => {
+        const wrapper = shallow(<Avatar stance={LEFT}/>);
+        expect(toJSON(wrapper)).toMatchSnapshot();
+    });
 
+    it('renders a right pointing emoji when given right stance', () => {
+        const wrapper = shallow(<Avatar stance={RIGHT}/>);
+        expect(toJSON(wrapper)).toMatchSnapshot();
+    });
+
+    it('renders a up pointing emoji when given up stance', () => {
+        const wrapper = shallow(<Avatar stance={UP}/>);
+        expect(toJSON(wrapper)).toMatchSnapshot();
+    });
+
+    it('renders a down pointing emoji when given down stance', () => {
+        const wrapper = shallow(<Avatar stance={DOWN}/>);
+        expect(toJSON(wrapper)).toMatchSnapshot();
+    });
 })
