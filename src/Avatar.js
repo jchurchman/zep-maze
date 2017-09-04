07941505b8da431114282c1a1651a77260fdaf30
Joe Churchman <churchman.joe@gmail.com>
2017-09-04 15:27:44 -0700

    avatar default test passes

---
+++ src/Avatar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {emojify} from 'react-emojione';
+import { UP, DOWN, LEFT, RIGHT } from './Avatar.constants';
+
+export function Avatar({ stance }) {
+    switch(stance) {
+        case UP: return ( <div> emojify( ':point_up_tone3:' ) </div> );
+        case DOWN: return ( <div> emojify( ':point_down_tone3:' ) </div> );
+        case RIGHT: return ( <div> emojify(':point_right_tone3:') </div> );
+        case LEFT: return ( <div> emojify( ':point_left_tone3:' ) </div> );
+        default: return ( <div> emojify( ':levitate_tone3:' ) </div>);
+    }
+}
