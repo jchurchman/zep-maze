8a1edd9b9525dceae8793a79379bd2b7798de7da
Joe Churchman <churchman.joe@gmail.com>
2017-09-05 11:24:22 -0700

    controller reducer move validation passes tests

--- src/Controller.reducer.js
+++ src/Controller.reducer.js
@@ -1,32 +1,36 @@
 import { UP, DOWN, LEFT, RIGHT } from './Controller.constants';
 
-export default ( state = { matrix: [], x: 0, y: 0 }, { type, payload }) => {
+export default ( state = { matrix: [], x: 1, y: 1 }, { type, payload }) => {
 	switch(type) {
-        case UP:
-            return {
-                ...state,
-                x: state.x,
-                y: state.y + 1
-            };
-        case DOWN:
-            return {
-                ...state,
-                x: state.x,
-                y: state.y - 1
-            }
-        case RIGHT: 
-            return {
-                ...state,
-                x: state.x + 1,
-                y: state.y
-            };
-        case LEFT:
-            return {
-                ...state,
-                x: state.x - 1,
-                y: state.y
-            };
-        default:
-            return state;
+	case UP:
+		if(state.matrix[ state.y-1][state.x] === 'WALL') return state;
+		return {
+			matrix: state.matrix,
+			x: state.x,
+			y: state.y - 1
+		};
+	case DOWN:
+		if(state.matrix[state.y+1][state.x] === 'WALL') return state;
+		return {
+			matrix: state.matrix,
+			x: state.x,
+			y: state.y + 1
+		};
+	case RIGHT:
+		if(state.matrix[state.y][state.x+1] === 'WALL') return state;
+		return {
+			matrix: state.matrix,
+			x: state.x + 1,
+			y: state.y
+		};
+	case LEFT:
+		if(state.matrix[state.y][state.x-1] === 'WALL') return state;
+		return {
+			matrix: state.matrix,
+			x: state.x - 1,
+			y: state.y
+		};
+	default:
+		return state;
 	}
 };
--- src/Controller.test.js
+++ src/Controller.test.js
@@ -1,30 +1,57 @@
 import React from 'react';
 import { goUP, goDOWN, goRIGHT, goLEFT } from './Controller.actions';
 import reducer from './Controller.reducer';
+import { WALL, FLOOR, START, END } from './Tile.constants';
 
+const maze = {
+    score: [],
+    matrix: [
+        [ WALL, WALL, WALL, WALL, WALL ],
+        [ WALL, START, WALL, END, WALL ],
+        [ WALL, FLOOR, WALL, FLOOR, WALL ],
+        [ WALL, FLOOR, FLOOR, FLOOR, WALL ],
+        [ WALL, WALL, WALL, WALL, WALL ],
+    ]
+}
 
 describe('Controller component', () => {
 
 	describe('reducer', () => {
 
 		it('has a default value of x: 0, y: 0', () => {
-			expect(reducer(undefined, {type: 'DUMMY' })).toEqual({ matrix: [], x: 0, y: 0});
+			expect(reducer(undefined, {type: 'DUMMY' })).toEqual({ matrix: [], x: 1, y: 1});
         });
         
-        it('goUP', () => {
-            expect(reducer({ matrix: [], x: 0, y: 0}, goUP())).toEqual({ matrix: [], x: 0, y: 1});
+        it('goUP does nothing on invalid move', () => {
+            expect(reducer({ matrix: maze.matrix, x: 1, y: 1}, goUP())).toEqual({ matrix: maze.matrix, x: 1, y: 1});
         });
         
-        it('goDOWN', () => {
-            expect(reducer({ matrix: [], x: 0, y: 0}, goDOWN())).toEqual({ matrix: [], x: 0, y: - 1});
+        it('goUP moves coordinates on valid move', () => {
+            expect(reducer({ matrix: maze.matrix, x: 1, y: 2}, goUP())).toEqual({ matrix: maze.matrix, x: 1, y: 1});
         });
         
-        it('goRIGHT', () => {
-            expect(reducer({ matrix: [], x: 0, y: 0}, goRIGHT())).toEqual({ matrix: [], x: 1, y: 0});
+        it('goDOWN moves coordinates on valid move', () => {
+            expect(reducer({ matrix: maze.matrix, x: 1, y: 1}, goDOWN())).toEqual({ matrix: maze.matrix, x: 1, y: 2});
+        });
+
+        it('goDOWN does nothing on invalid move', () => {
+            expect(reducer({ matrix: maze.matrix, x: 1, y: 3}, goDOWN())).toEqual({ matrix: maze.matrix, x: 1, y: 3});
         });
         
-        it('goLEFT', () => {
-            expect(reducer({ matrix: [], x: 0, y: 0}, goLEFT())).toEqual({ matrix: [], x: - 1, y: 0 });
+        it('goRIGHT does nothing on invalid move', () => {
+            expect(reducer({ matrix: maze.matrix, x: 1, y: 1}, goRIGHT())).toEqual({ matrix: maze.matrix, x: 1, y: 1});
+        });
+        
+        it('goRIGHT moves coordinates on valid move', () => {
+            expect(reducer({ matrix: maze.matrix, x: 1, y: 3}, goRIGHT())).toEqual({ matrix: maze.matrix, x: 2, y: 3});
+        });
+        
+        it('goLEFT does nothing on invalid move', () => {
+            expect(reducer({ matrix: maze.matrix, x: 1, y: 1}, goLEFT())).toEqual({ matrix: maze.matrix, x: 1, y: 1 });
+        });
+        
+        it('goLEFT moves coordinates on valid move', () => {
+            expect(reducer({ matrix: maze.matrix, x: 2, y: 3}, goLEFT())).toEqual({ matrix: maze.matrix, x: 1, y: 3 });
         });
 	});
 });
