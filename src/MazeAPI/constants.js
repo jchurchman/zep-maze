617f9c7f7cd6f5d9959925b35df3a4ac5b2a702f
Joe Churchman <churchman.joe@gmail.com>
2017-09-05 17:50:23 -0700

    began work on mazeAPI

---
+++ src/MazeAPI/constants.js
@@ -0,0 +1,8 @@
+export const FETCHING_MAZES = 'FETCHING_MAZES';
+export const FETCHING_MAZE = 'FETCHING_MAZE';
+export const FETCHED_MAZES = 'FETCHED_MAZES';
+export const FETCHED_MAZE = 'FETCHED_MAZE';
+export const FETCHED_MAZE_ERROR = 'FETCHED_MAZE_ERROR';
+export const ADDING_MAZE = 'ADDING_MAZE';
+export const ADDED_MAZE = 'ADDED_MAZE';
+export const ADDING_MAZE_ERROR = 'ADDING_MAZE_ERROR';
--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import registerServiceWorker from './registerServiceWorker';
 import thunk from 'redux-thunk';
 import ControllerReducer from './Controller/Controller.reducer';
+import moveChecker from './Controller/Controller.middleware';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
