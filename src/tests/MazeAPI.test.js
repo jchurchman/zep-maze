784e93a94f5481ae913b85d75640c923250617c0
Joe Churchman <churchman.joe@gmail.com>
2017-09-12 11:06:49 -0700

    getOneMaze API action passes test

---
+++ src/tests/MazeAPI.test.js
@@ -0,0 +1,34 @@
+import { 
+    makeAddMaze, 
+    makeGetOneMaze 
+    } from '../MazeAPI/MazeAPI.actions';
+import { 
+    FETCHED_MAZE, 
+    FETCHING_MAZE,
+    FETCHED_MAZE_ERROR,
+    ADDED_MAZE,
+    ADDING_MAZE,
+    ADDING_MAZE_ERROR
+    } from '../MazeAPI/constants';
+
+describe('getOneMaze', () => {
+
+    it('successful get one maze', () => {
+
+        const maze = {};
+        const api = { get: () => Promise.resolve(maze) };
+        const dispatched = [];
+        const dispatch = (action) => dispatched.push(action);
+
+        const getOneMaze = makeGetOneMaze(api);
+        return getOneMaze()(dispatch)
+            .then( () => {
+                expect(dispatched).toEqual([
+                    { type: FETCHING_MAZE },
+                    { type: FETCHED_MAZE, payload: maze }
+                ]);
+            });
+    });
+
+
+})
