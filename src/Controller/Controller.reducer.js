48abc55fd061c9877f74c4ecb382372ada4abf25
Joe Churchman <churchman.joe@gmail.com>
2017-09-25 10:39:01 -0700

    redux tests written, passing

--- src/Controller/Controller.reducer.js
+++ src/Controller/Controller.reducer.js
@@ -19,38 +19,3 @@ export const gamestate = (state = 'ACTIVE', { type, payload }) => {
 		return state;
 	}
 };
-
-// export default ( state = { matrix: maze.matrix, x: 1, y: 1 }, { type }) => {
-// 	switch(type) {
-// 	case UP:
-// 		if(state.matrix[ state.y-1][state.x] === 'WALL') return state;
-// 		return {
-// 			matrix: state.matrix,
-// 			x: state.x,
-// 			y: state.y - 1
-// 		};
-// 	case DOWN:
-// 		if(state.matrix[state.y+1][state.x] === 'WALL') return state;
-// 		return {
-// 			matrix: state.matrix,
-// 			x: state.x,
-// 			y: state.y + 1
-// 		};
-// 	case RIGHT:
-// 		if(state.matrix[state.y][state.x+1] === 'WALL') return state;
-// 		return {
-// 			matrix: state.matrix,
-// 			x: state.x + 1,
-// 			y: state.y
-// 		};
-// 	case LEFT:
-// 		if(state.matrix[state.y][state.x-1] === 'WALL') return state;
-// 		return {
-// 			matrix: state.matrix,
-// 			x: state.x - 1,
-// 			y: state.y
-// 		};
-// 	default:
-// 		return state;
-// 	}
-// };
