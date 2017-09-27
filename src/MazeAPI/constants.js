96f5e122fa1751128bec83877d4c1aa72593e43a
Joe Churchman <churchman.joe@gmail.com>
2017-09-27 12:27:49 -0700

    uncommented maze fetching code

--- src/MazeAPI/constants.js
+++ src/MazeAPI/constants.js
@@ -1,6 +1,6 @@
-// export const FETCHING_MAZES = 'FETCHING_MAZES';
+export const FETCHING_MAZES = 'FETCHING_MAZES';
 export const FETCHING_MAZE = 'FETCHING_MAZE';
-// export const FETCHED_MAZES = 'FETCHED_MAZES';
+export const FETCHED_MAZES = 'FETCHED_MAZES';
 export const FETCHED_MAZE = 'FETCHED_MAZE';
 export const FETCHED_MAZE_ERROR = 'FETCHED_MAZE_ERROR';
 export const ADDING_MAZE = 'ADDING_MAZE';
