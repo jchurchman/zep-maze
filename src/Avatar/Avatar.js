5380f6a7f103f34333d2e2c924c31a18aacff126
Joe Churchman <churchman.joe@gmail.com>
2017-10-10 15:00:32 -0700

    converted Avatar.js to styled components, removed css files

--- src/Avatar/Avatar.js
+++ src/Avatar/Avatar.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import  { emojify } from 'react-emojione';
-import styles from './Avatar.css'
+import styled from 'styled-components';
+
+const Sprite = styled.div`
+    position: absolute;
+    top: 0;
+    right: 0;
+    left: 0;
+    bottom: 0;
+    padding: auto;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: 10000;
+`
 
 export default function Avatar() {
     
-    return ( <div className={styles.avatar}> {emojify(':wink:', { style: {top: 0}})} </div>);
+    return ( 
+        <Sprite> 
+            {emojify(':wink:', { style: {top: 0}})} 
+        </Sprite>
+    );
 }
