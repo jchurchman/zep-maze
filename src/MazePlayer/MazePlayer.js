8599005643d1278283636344a8aa09aab0d398dd
Joe Churchman <churchman.joe@gmail.com>
2017-10-10 14:58:17 -0700

    remved import of deleted files in mazePlayer

--- src/MazePlayer/MazePlayer.js
+++ src/MazePlayer/MazePlayer.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import styles from './MazePlayer.css';
 import MazeMapper from '../MazeMapper/MazeMapper';
 import Avatar from '../Avatar/Avatar';
 import ControllerContainer from '../Controller/ControllerContainer';
